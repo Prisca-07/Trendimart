235696668239adefd6d0cb16ccd82c7c797c3655
Priscilla Obadoni <115809303+Prisca-07@users.noreply.github.com>
2025-09-27 13:37:24 -0700

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,41 @@
+SELECT * FROM [TRENDIMART -MySQL]
+--TOTAL REVENUE
+SELECT SUM(Revenue) AS TOTALREVENUE FROM [TRENDIMART -MySQL]
+/* The Total Revenue of TRENDIMART is N317,100 */
+
+
+--NET PROFIT
+SELECT SUM(Profit) AS NETPROFIT FROM [TRENDIMART -MySQL]
+/*The NET PROFIT of the TRENDIMART is N100,000 */
+
+--TOTAL CUSTOMERS
+SELECT COUNT(SN) AS TOTAL_CUSTOMERS FROM [TRENDIMART -MySQL]
+/* TRENDIMART have a total of 50 Customers */
+
+--REVENUE PER DAY
+SELECT Date, SUM(Revenue) AS Revenue_per_day 
+FROM [TRENDIMART -MySQL]
+GROUP BY Date
+/* These are the following revenue per day at TRENDIMART
+2025-01-05	N87500, 2025-01-06	N25500, 2025-01-07	N27100, 2025-01-08	N44500,
+2025-01-09	N19100, 2025-01-10	N58000, 2025-01-11	N17500, 2025-01-12	N12500,
+2025-01-14	N3000, 2025-01-15	N12600, 2025-01-16	N600, 2025-01-17	N600, 
+2025-01-18	N6200,2025-01-19	N600, 2025-01-20	N600, 2025-01-21	N600, 2025-01-22	N600 */
+
+
+--production distribution
+SELECT Product_Category, COUNT(Product_Category) as Product_Distribution
+FROM [TRENDIMART -MySQL]
+GROUP BY Product_Category
+/* TRENDIMART Product category are as follow 
+Electronics	18, Fashion	17, Groceries	15 */
+
+--unit sold
+SELECT SUM(Units_Sold) AS UNIT_SOLD FROM [TRENDIMART -MySQL]
+/* The total unit sold in TRENDIMART is 1905 units of product */
+
+--TOP PRODUCT
+SELECT TOP 5(Product_Name)
+FROM [TRENDIMART -MySQL]
+
+/* The top product is Smartphone X */
